a525f32fe3f55eeef5552f242d7cf1909d1d57d1
get <45425365+Minty-Meeo@users.noreply.github.com>
2023-12-10 08:47:18 -0600

    TEX2TXG blue 255 -> alpha conversion

--- scg_tools/TEX2TXG.py
+++ scg_tools/TEX2TXG.py
@@ -13,6 +13,12 @@ def help(progname: str) -> None:
           f"Usage: {progname} [PSXtexfile path] [GCMaterials path]")
 #
 
+def blue_to_alpha(pixel: tuple[int]):
+    if pixel == (0, 0, 255, 255):
+        return (0, 0, 255, 0)
+    return pixel
+#
+
 def main() -> int:
     progname = path.basename(argv[0])
     if len(argv) < 3:
@@ -26,7 +32,11 @@ def main() -> int:
     
     gcmaterials = list[GCMaterial]()
     for [mode, unk1, unk2, width, height, data, palette] in psx_textures:
-        gcmaterials.append(GCMaterial.encode(decode_psxtexfile_solo(mode, data, palette, width, height), unk1))
+        image = decode_psxtexfile_solo(mode, data, palette, width, height)
+        if image.mode != "RGBA":
+            image = image.convert("RGBA")  # There has got to be a faster way to do this...
+            image.putdata([blue_to_alpha(pixel) for pixel in image.getdata()])
+        gcmaterials.append(GCMaterial.encode(image, unk1))
 
     with open_helper(txg_path, "wb", make_dirs = True, overwrite = True) as f:
         write_gcmaterials(f, gcmaterials)
